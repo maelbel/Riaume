eeb9cf3da60a13d775e97f1e006110ef28bd1632
Mael <90256483+maelbel@users.noreply.github.com>
2023-11-07 16:05:56 +0100

    Start welcome page

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,11 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::middleware('auth')->group(function () {
-    Route::get('/', function () {
-        return view('welcome');
-    });
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
